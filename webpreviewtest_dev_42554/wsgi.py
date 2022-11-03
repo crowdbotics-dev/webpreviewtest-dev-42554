9219bf23a80b380dcfc2af04d4b9e3c4b7249779
Crowdbotics <engineering@crowdbotics.com>
2022-11-03 07:16:18 +0000

    Initial commit.

---
+++ webpreviewtest_dev_42554/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for webpreviewtest_dev_42554 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webpreviewtest_dev_42554.settings')
+
+application = get_wsgi_application()
